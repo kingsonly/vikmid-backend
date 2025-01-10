d6ab2a8c1aac38fc383acab10b678215419603fe
kingsley achumie <kingsonly13c@gmail.com>
2025-01-10 23:30:18 +0100

    wip

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! its kingsonly2';
+    return 'Hello World! its kingsonly3';
   }
 }
