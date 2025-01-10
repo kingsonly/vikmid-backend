a0404ab718b1ce0bd9fcd408135a692f9c870455
kingsley achumie <kingsonly13c@gmail.com>
2025-01-10 22:56:47 +0100

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PlansModule } from './plans/plans.module';
 import { WaitlistController } from './waitlist/waitlist.controller';
 import { WaitlistModule } from './waitlist/waitlist.module';
@@ -16,15 +16,19 @@ import { WaitlistModule } from './waitlist/waitlist.module';
     ConfigModule.forRoot({
       isGlobal: true, // Makes the ConfigModule globally available
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql', // or your chosen database
-      host: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: 'Ubuxa##99',
-      database: 'forge',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Import ConfigModule to provide ConfigService
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // or your chosen database
+        host: configService.get<string>('DB_HOST', 'mysql'), // Default to 'mysql' if not set
+        port: configService.get<number>('DB_PORT', 3306), // Default to 3306 if not set
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD', 'Ubuxa##99'), // Default value provided
+        database: configService.get<string>('DB_NAME', 'forge'), // Default value provided
+        autoLoadEntities: true,
+        synchronize: true, // Should be false in production to avoid accidental schema changes
+      }),
+      inject: [ConfigService], // Inject ConfigService
     }),
     PlansModule,
     WaitlistModule,
