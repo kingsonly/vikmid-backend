a1da2ffeec6e9f7978487ffdd99d8159c978f6a6
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2025-01-12 10:23:54 +0100

    feat: implement add a product feature

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './entities/product.entity';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  // @Get()
+  // @ApiOperation({ summary: 'Get all products' })
+  // async getAllProducts(): Promise<Product[]> {
+  //   return this.productsService.getAllProduct();
+  // }
+
+  @Post()
+  @ApiOperation({ summary: 'Create a new product' })
+  async createProduct(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<Product> {
+    return this.productsService.createProduct(createProductDto);
+  }
+}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { Product } from './entities/product.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductRepository } from './products.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService, ProductRepository],
 })
 export class ProductsModule {}
