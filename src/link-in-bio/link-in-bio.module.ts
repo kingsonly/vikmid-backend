b6758f61ddc3026ae311a76f087514ad3a134d0f
kingsley achumie <kingsonly13c@gmail.com>
2025-02-23 13:58:16 +0100

    wip

--- src/link-in-bio/link-in-bio.module.ts
+++ src/link-in-bio/link-in-bio.module.ts
@@ -12,6 +12,8 @@ import { SocialLinkStats } from './entity/social-link-stats.entity';
 import { SocialLinks } from './entity/social-links.entity';
 import { StorageService } from 'config/storage.provider';
 import { HubModule } from 'src/hub/hub.module';
+import { LinksController } from './links/links.controller';
+import { LinksService } from './links/links.service';
 
 @Module({
   imports: [
@@ -29,7 +31,7 @@ import { HubModule } from 'src/hub/hub.module';
       ]),
     HubModule
   ],
-  providers: [LinkInBioService, StorageService,],
-  controllers: [LinkInBioController]
+  providers: [LinkInBioService, StorageService, LinksService,],
+  controllers: [LinkInBioController, LinksController]
 })
 export class LinkInBioModule { }
